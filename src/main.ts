990cf6440ef1daf51a3e9a2161f86fb1b2487381
Yonathan Meyerzon <yonathan.meyerzon@gmail.com>
2024-02-28 14:28:12 +0200

    config for github pages 3

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
+/// <reference types="vite/client" />
+declare global {
+  interface Window {
+    openPopUp: (/* arguments of summonPopUp function */) => void;
+    closePopUp: (/* arguments of summonPopUp function */) => void;
+  }
+}
+
 import "./style.css";
 
 const popUp = document.querySelector(".sliding-pop-up");
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,1 @@
 /// <reference types="vite/client" />
-declare global {
-  interface Window {
-    openPopUp: (/* arguments of summonPopUp function */) => void;
-    closePopUp: (/* arguments of summonPopUp function */) => void;
-  }
-}
